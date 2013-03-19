8f3222e181d4720ad859583506b174086e885fed
Yevgen <godun.yevgen@gmail.com>
2013-03-19 13:17:57 +0200

    2 cols

--- js/common.js
+++ js/common.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
 
+// ------------------- View type ------------------- //
+$(".js-view-type").click(function(){
+    var list = $(this).parent().children(".js-drop-down");
+    if($(this).hasClass("is-active")) {
+        $(this).removeClass("is-active");
+        list.slideUp("fast");
+    }
+    else {
+        $(this).addClass("is-active");
+        list.slideDown("fast");
+    }
+});
+
+
+
+
 });
